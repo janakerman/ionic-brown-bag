0f3c3dc1df947683006488699693a7aa20ec3d3b
Jan Akerman <jakerman@scottlogic.com>
2015-07-27 11:56:07 +0100

    Add delete & reorder functionality to the contacts list

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -3,8 +3,30 @@ angular.module('starter.controllers', [])
   .controller('ContactListCtrl', function(contactService, $scope) {
 
     $scope.data = {
+      showDelete: false,
       contacts: contactService.getContacts()
     };
 
-  });
+    // Delete
 
+    $scope.deleteClicked = function() {
+      $scope.data.showDelete = !$scope.data.showDelete;
+    };
+
+    $scope.onContactDelete = function(contact) {
+      // Remove the deleted item from our model.
+      $scope.data.contacts.splice($scope.data.contacts.indexOf(contact), 1);
+    };
+
+    // Reorder
+
+    $scope.reorderClicked = function() {
+      $scope.data.showReorder = !$scope.data.showReorder;
+    };
+
+    $scope.onContactReorder = function(contact, fromIndex, toIndex) {
+      // Reorder the deleted item in our model
+      $scope.data.contacts.splice(fromIndex, 1);
+      $scope.data.contacts.splice(toIndex, 0, contact);
+    }
+});
